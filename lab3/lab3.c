3559df9598423e8aa0c82bd1cddc7295d38f4912
Daniil Oleynik <lfuuu@yandex.ru>
2020-09-16 22:20:14 +0300

    lab3

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,14 +1,52 @@
 #include <stdio.h>
 #include <math.h>
 
-void main ()
+double input_val(char *str, int fun(double))
 {
-    double a, b, c, D;
-    scanf("%lf%lf%lf", &a, &b, &c);
+    int res = 1;
+    double par;
 
-    D = pow(b,2) - 4 * a * c;
+    while (res)
+    {
+        printf("%s", str);
+        if (scanf("%lf", &par) == 1) // Количество правильно введенных аргументов
+        {
+            if (!fun(par))
+            { // Вызываем проверочную функцию по адресу fun
+                res = 0;
+            }
+            else
+            {
+                printf("Ошибка\n");
+            }
+        }
+    }
+    return par;
+}
 
-    if (D < 0) 
+int checkA(double par)
+{
+    if (par == 0) // return par == 0 ? -1 : 0;
+        return -1;
+    else
+        return 0;
+}
+
+int checkBC(double par)
+{
+    return 0;
+}
+
+void main()
+{
+    printf("Решение уравнения A*x^2+B*x+C=0\n");
+    double a = input_val("Введите A (не ноль):", checkA);
+    double b = input_val("Введите B:", checkBC);
+    double c = input_val("Введите C:", checkBC);
+
+    double D = pow(b, 2) - 4 * a * c;
+
+    if (D < 0)
     {
         double b1 = -b / 2 * a, D1 = sqrt(fabs(D));
         printf("x1 =  %f - i * %f\n", b1, D1);
@@ -16,8 +54,7 @@ void main ()
     }
     else
     {
-        double x1 = (-b - sqrt(D)) / (2 * a), x2 = (-b +sqrt(D)) / (2 * a);
-        printf("x1 = %f\nx2 = %f\n", x2, x1);    
+        double x1 = (-b - sqrt(D)) / (2 * a), x2 = (-b + sqrt(D)) / (2 * a);
+        printf("x1 = %f\nx2 = %f\n", x1, x2);
     }
-    
 }
